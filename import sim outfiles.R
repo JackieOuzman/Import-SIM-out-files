6cd08d3d865f59e997afc1475865bd9df6f84b8b
JackieOuzman <jackie.ouzman@csiro.au>
2023-10-13 09:07:34 +1030

    working but watch the import function it doesn't always do what I expect it to do.
    Next steps is to all all the other factoral

--- import sim outfiles.R
+++ import sim outfiles.R
@@ -24,7 +24,7 @@ list_sim_out_file <-
   )
 
 list_sim_out_file
-file_name <- list_sim_out_file[1]
+file_name <- list_sim_out_file[2]
 file_name
 
 ## create a empty data frame
@@ -39,6 +39,9 @@ df_for_all_data <- data.frame(
   InitalWater       = character() #add more here
 )
 
+
+# df <- read_table(paste0(file_directory,"/",
+#                         file_name), col_names = FALSE)
 ################################################################################
 ###############################################################################
 ##########               As a loop                                    ##########
@@ -102,38 +105,52 @@ df_for_all_data <- rbind(df_for_all_data, all_data)
 
 
 
-# df_for_all_data$Weed_emergedate <- as.numeric(df_for_all_data$Weed_emergedate)
-# 
-# df_for_all_data$origin <- as.Date(paste0(df_for_all_data$year, "-01-01"),tz = "UTC") - days(1)
-# df_for_all_data <- df_for_all_data %>% 
-#   mutate(Weed_emergedate_date =  as.Date(df_for_all_data$Weed_emergedate , origin = df_for_all_data$origin, tz = "UTC"))
-# df_for_all_data <- df_for_all_data %>% 
-#   mutate(Weed_emergedate_date = case_when(
-#     Weed_emergedate > 0 ~ Weed_emergedate_date,
-#     TRUE ~ NA_real_
-#   ))
-# 
-# df_for_all_data <- df_for_all_data %>% 
-#   mutate(#Day = day(Weed_emergedate_date),
-#          Month = month(Weed_emergedate_date),
-#          DayMonth = format(as.Date(Weed_emergedate_date), "%d-%m"))
-# 
-# 
-# df_for_all_data <- df_for_all_data %>% 
-#   mutate(weed_sow_window = case_when(
-#     Month==12 ~ "Dec",
-#     Month==1 ~ "Jan",
-#     Month==2 ~ "Feb",
-#     Month==3 ~ "March",
-#     TRUE ~ "NA"
-#   ))
-
-
-
 
 }
 
-write.csv(df_for_all_data, paste0(file_directory, "/merge_sim_output.csv"), row.names = FALSE)
+
+#################################################################################
+### add extra info on the day of emergence
+df_for_all_data$Weed_emergedate <- as.numeric(df_for_all_data$Weed_emergedate)
+
+df_for_all_data$origin <- as.Date(paste0(df_for_all_data$year, "-01-01"),tz = "UTC") - days(1)
+df_for_all_data <- df_for_all_data %>%
+  mutate(Weed_emergedate_date =  as.Date(df_for_all_data$Weed_emergedate , origin = df_for_all_data$origin, tz = "UTC"))
+df_for_all_data <- df_for_all_data %>%
+  mutate(Weed_emergedate_date = case_when(
+    Weed_emergedate > 0 ~ Weed_emergedate_date,
+    TRUE ~ NA_real_
+  ))
+
+df_for_all_data <- df_for_all_data %>%
+  mutate(#Day = day(Weed_emergedate_date),
+    Month = month(Weed_emergedate_date),
+    DayMonth = format(as.Date(Weed_emergedate_date), "%d-%m"))
+
+
+df_for_all_data <- df_for_all_data %>%
+  mutate(weed_sow_window = case_when(
+    Month==12 ~ "Dec",
+    Month==1 ~ "Jan",
+    Month==2 ~ "Feb",
+    Month==3 ~ "March",
+    TRUE ~ as.character(NA_real_)
+  ))
+
+
+#################################################################################
+### save the file
+
+### select clms I want
+names(df_for_all_data)
+
+df_for_all_data <- df_for_all_data %>% 
+  select(- origin,
+         -Month,              
+         -DayMonth )
+
+
+write.csv(df_for_all_data, paste0(file_directory, "/merged/merge_sim_output.csv"), row.names = FALSE)
 
 
 

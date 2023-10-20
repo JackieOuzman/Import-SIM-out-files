c541aeeff156a1fb63460796c954f512b7531070
JackieOuzman <jackie.ouzman@csiro.au>
2023-10-20 16:45:25 +1030

    taking forever to run - need to try on remote computer

--- Step1_import sim outfiles.R
+++ Step1_import sim outfiles.R
@@ -25,8 +25,8 @@ list_sim_out_file <-
   )
 
 list_sim_out_file
-file_name_temp_list <- list_sim_out_file[c(1,2)]
-file_name <- list_sim_out_file[1]
+#file_name_temp_list <- list_sim_out_file[c(1,2)]
+#file_name <- list_sim_out_file[1]
 
 ## create a empty data frame
 #str(all_data)
@@ -90,12 +90,12 @@ heading <- c(
 
 
 
-for (file_name_temp_list in file_name_temp_list){
+for (list_sim_out_file in list_sim_out_file){
 
 
 
 df <- read_table(paste0(file_directory,"/",
-                        file_name_temp_list ), #change this back to list when your ready to run as loop (list_sim_out_file OR file_name
+                        list_sim_out_file ), #change this back to list when your ready to run as loop (list_sim_out_file OR file_name
                  col_names = FALSE, skip = 5)
 
 
@@ -104,13 +104,13 @@ colnames(df)<- heading # add the column headings (as set above - before the loop
 ### pull out the simulation infomation so I can create some new clms
 
 version = read.csv(paste0(file_directory,"/",
-                          file_name_temp_list), skip = 0, header = F, nrows = 1, as.is = T)
+                          list_sim_out_file), skip = 0, header = F, nrows = 1, as.is = T)
 
 factors = read.csv(paste0(file_directory,"/",
-                          file_name_temp_list), skip = 1, header = F, nrows = 1, as.is = T)
+                          list_sim_out_file), skip = 1, header = F, nrows = 1, as.is = T)
 
 title_a = read.csv(paste0(file_directory,"/",
-                          file_name_temp_list), skip = 2, header = F, nrows = 1, as.is = T)
+                          list_sim_out_file), skip = 2, header = F, nrows = 1, as.is = T)
 
 
 ### formatting the above information
@@ -182,48 +182,19 @@ df_for_all_data <- rbind(df_for_all_data, df)
 }
 
 
-#################################################################################
-### add extra info on the day of emergence
-df_for_all_data$Weed_emergedate <- as.numeric(df_for_all_data$Weed_emergedate)
-
-df_for_all_data$origin <- as.Date(paste0(df_for_all_data$year, "-01-01"),tz = "UTC") - days(1)
-df_for_all_data <- df_for_all_data %>%
-  mutate(Weed_emergedate_date =  as.Date(df_for_all_data$Weed_emergedate , origin = df_for_all_data$origin, tz = "UTC"))
-df_for_all_data <- df_for_all_data %>%
-  mutate(Weed_emergedate_date = case_when(
-    Weed_emergedate > 0 ~ Weed_emergedate_date,
-    TRUE ~ NA_real_
-  ))
-
-df_for_all_data <- df_for_all_data %>%
-  mutate(#Day = day(Weed_emergedate_date),
-    Month = month(Weed_emergedate_date),
-    DayMonth = format(as.Date(Weed_emergedate_date), "%d-%m"))
-
-
-df_for_all_data <- df_for_all_data %>%
-  mutate(weed_sow_window = case_when(
-    Month==12 ~ "Dec",
-    Month==1 ~ "Jan",
-    Month==2 ~ "Feb",
-    Month==3 ~ "March",
-    TRUE ~ as.character(NA_real_)
-  ))
 
 
 #################################################################################
 ### save the file
 
 ### select clms I want
-names(df_for_all_data)
+names(df)
 
-df_for_all_data <- df_for_all_data %>% 
-  select(- origin,
-         -Month,              
-         -DayMonth )
+site <- "Katanning"
 
 
-write.csv(df_for_all_data, paste0(file_directory, "/merged/merge_sim_output.csv"), row.names = FALSE)
+
+write.csv(df_for_all_data, paste0("X:/Summer_weeds/APSIM_7/WA_sites/output_csv/", site, ".csv"), row.names = FALSE)
 
 
 

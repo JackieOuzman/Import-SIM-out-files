3ab6a3989334801411713ea8afac6b7e8dbee7a0
JackieOuzman <jackie.ouzman@csiro.au>
2023-10-23 09:12:10 +1030

    step 2

--- Step2_import sim outfiles_with_extra_formats.R
+++ Step2_import sim outfiles_with_extra_formats.R
@@ -5,29 +5,35 @@ library(readr)
 library(stringr)
 library(lubridate)
 
+#################################################################################
+## import data
+site <- "Katanning"
+df <- read.csv(paste0("X:/Summer_weeds/APSIM_7/WA_sites/output_csv/", site, ".csv"))
 
+
+names(df)
 
 
 #################################################################################
 ### add extra info on the day of emergence
-df_for_all_data$Weed_emergedate <- as.numeric(df_for_all_data$Weed_emergedate)
+df$Weed_emergedate <- as.numeric(df$Weed_emergedate)
 
-df_for_all_data$origin <- as.Date(paste0(df_for_all_data$year, "-01-01"),tz = "UTC") - days(1)
-df_for_all_data <- df_for_all_data %>%
-  mutate(Weed_emergedate_date =  as.Date(df_for_all_data$Weed_emergedate , origin = df_for_all_data$origin, tz = "UTC"))
-df_for_all_data <- df_for_all_data %>%
+df$origin <- as.Date(paste0(df$year, "-01-01"),tz = "UTC") - days(1)
+df <- df %>%
+  dplyr::mutate(Weed_emergedate_date =  as.Date(df$Weed_emergedate , origin = df$origin, tz = "UTC"))
+df <- df %>%
   mutate(Weed_emergedate_date = case_when(
     Weed_emergedate > 0 ~ Weed_emergedate_date,
     TRUE ~ NA_real_
   ))
 
-df_for_all_data <- df_for_all_data %>%
-  mutate(#Day = day(Weed_emergedate_date),
+df <- df %>%
+  mutate(
     Month = month(Weed_emergedate_date),
     DayMonth = format(as.Date(Weed_emergedate_date), "%d-%m"))
 
 
-df_for_all_data <- df_for_all_data %>%
+df <- df %>%
   mutate(weed_sow_window = case_when(
     Month==12 ~ "Dec",
     Month==1 ~ "Jan",
@@ -36,20 +42,25 @@ df_for_all_data <- df_for_all_data %>%
     TRUE ~ as.character(NA_real_)
   ))
 
-
-#################################################################################
-### save the file
-
-### select clms I want
-names(df_for_all_data)
-
-df_for_all_data <- df_for_all_data %>% 
+### tidy up
+df <- df %>% 
   select(- origin,
          -Month,              
          -DayMonth )
 
 
-write.csv(df_for_all_data, paste0(file_directory, "/merged/merge_sim_output.csv"), row.names = FALSE)
+
+
+
+
+
+
+#################################################################################
+### save the file
+
+### select clms I want
+names(df)
+write.csv(df, paste0("X:/Summer_weeds/APSIM_7/WA_sites/output_csv/", site, "extra_Clm_step2.csv"), row.names = FALSE)
 
 
 

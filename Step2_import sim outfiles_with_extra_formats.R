cd8ad79a36fe1548f92536cd87ea43ea6b8bb427
Ouzman <ouz001@csiro.au>
2023-11-14 08:35:29 +1100

    Roseworthy

--- Step2_import sim outfiles_with_extra_formats.R
+++ Step2_import sim outfiles_with_extra_formats.R
@@ -17,7 +17,9 @@ library(lubridate)
 #site <-"Meckering_top_up"
 #site <-"Merredin_top_up"
 #site <- "Birchip/Birchip_top_up"
-site <- "Loxton_top_up"
+#site <- "Loxton_top_up"
+
+site <- "Roseworthy_top_up"
 
 df <- read.csv(paste0("X:/Summer_weeds/APSIM_7/SA_sites/output_csv/", site, "_all.csv"))
 
